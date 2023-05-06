7654be23393283a9e4a821afd15c8f82049a4351
Raúl Ruiz <raul.ruizpedrosa@Outlook.com>
2023-05-06 20:28:59 +0200

    Exercici 1 acabat

---
+++ src/main/java/Ruiz/Raul/itacademy/acces/especialitzacio/java/exercici1/Document.java
@@ -0,0 +1,59 @@
+package Ruiz.Raul.itacademy.acces.especialitzacio.java.exercici1;
+
+import java.util.ArrayList;
+
+public class Document {
+
+    private ArrayList<String> paragrafs;
+    private String titol;
+
+    private int nombreParagrafs;
+
+    public Document() {
+        paragrafs = new ArrayList<>();
+    }
+
+    public ArrayList<String> getParagrafs() {
+        return paragrafs;
+    }
+
+    public void setParagrafs(ArrayList<String> paragrafs) {
+        this.paragrafs = paragrafs;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public int getNombreParagrafs() {
+        // Devolvemos el tamaño del arrai para saber el numero de paragrafos que tenemos en la lista
+        return paragrafs.size();
+    }
+
+    public String getParagraf(int posicio) {
+
+        // Si la posición es mayor o igual al tamaño del array significa que no tenemos esa posición
+        if (paragrafs.size() <= posicio) {
+            throw new RuntimeException("No hi ha cap paràgraf a la posició indicada");
+        }
+
+        return paragrafs.get(posicio);
+
+    }
+
+    public void setParagraf(int posicio, String paragraf) {
+
+        // Si la posicion indicada ya existe en la lista, reemplazamos el valor por el nuevo, sino, la añadimos
+        if (posicio < paragrafs.size()) {
+            paragrafs.set(posicio, paragraf);
+        } else {
+            paragrafs.add(paragraf);
+        }
+
+    }
+
+}
---
+++ src/main/java/Ruiz/Raul/itacademy/acces/especialitzacio/java/exercici1/Inici.java
@@ -0,0 +1,44 @@
+package Ruiz.Raul.itacademy.acces.especialitzacio.java.exercici1;
+
+import java.util.ArrayList;
+
+public class Inici {
+
+    public static void main(String[] args) {
+
+        Document documentTest = new Document();
+        ArrayList<String> paragrafs = new ArrayList<>();
+
+        System.out.println(documentTest.getNombreParagrafs()); //Mostra el resultat a la consola
+
+        documentTest.setParagraf(0, "Text 0");
+        documentTest.setParagraf(1, "Text 1");
+        documentTest.setParagraf(2, "Text 2");
+        documentTest.setParagraf(3, "Text 3");
+
+        System.out.println(documentTest.getParagraf(1)); //Mostra el resultat a la consola
+
+        try {
+            System.out.println(documentTest.getParagraf(5)); //Tracta l’excepció mostrant el missatge d’error a la consola
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        documentTest.setParagraf(2, "Text 2 modificat");
+
+        /*Ara, guarda el retorn del mètode documentTest.getParagrafs() a una
+        variable del tipus adient, i recorre-la amb un bucle for mostrant per
+        consola cada ocurrència, demostrant que el contingut final del
+        document és:
+        Text 0
+        Text 1
+        Text 2 modificat
+        Text 3*/
+
+        paragrafs = documentTest.getParagrafs();
+
+        paragrafs.forEach(System.out::println);
+
+    }
+
+}
